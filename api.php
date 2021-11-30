d928296d223b575036f5d5b19cc63e51ecced4e4
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-30 11:19:37 -0500

    21.11-dev-2

--- api.php
+++ api.php
@@ -1,2 +1,18 @@
 <?php
-class urlAPI extends API {}
+class urlAPI extends API {
+  public function load($request = null, $data = null){
+    if(isset($data)){
+			if(!is_array($data)){ $data = json_decode($data, true); }
+      return [
+        "success" => $this->Language->Field["Record successfull"],
+        "request" => $request,
+        "data" => $data,
+        "output" => [
+          "plugin" => $this->Settings['plugins']['url']['settings']['plugin'],
+          "view" => $this->Settings['plugins']['url']['settings']['view'],
+          "id" => $this->Settings['plugins']['url']['settings']['id'],
+        ],
+      ];
+    }
+  }
+}
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,12 @@
 API.Plugins.url = {
 	init:function(){},
 	load:{
-		index:function(){},
+		index:function(){
+			API.request(url.searchParams.get("p"),'load',function(result){
+				var dataset = JSON.parse(result);
+				if(dataset.success != undefined){}
+			});
+		},
 	},
 }
 
