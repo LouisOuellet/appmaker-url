fe09e729052a059c715fcfc8a17e7ae9b57be902
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-30 09:36:06 -0500

    21.11-dev-1

---
+++ dist/js/script.js
@@ -0,0 +1,8 @@
+API.Plugins.url = {
+	init:function(){},
+	load:{
+		index:function(){},
+	},
+}
+
+API.Plugins.url.init();
---
+++ src/views/index.php
@@ -0,0 +1,2 @@
+<link rel="preload" href="plugins/events/dist/fonts/Scriptina.woff2" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="plugins/events/dist/fonts/SignPainterHouseScript.woff2" as="font" type="font/woff2" crossorigin>
