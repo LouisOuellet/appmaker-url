1091355688634eff45484c352daabe4991b4395e
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-30 11:40:35 -0500

    21.11-dev-4

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,7 @@ API.Plugins.url = {
 	init:function(){},
 	load:{
 		index:function(){
-			API.request(url.searchParams.get("p"),'load',function(result){
+			API.request('url','load',function(result){
 				var dataset = JSON.parse(result);
 				if(dataset.success != undefined){}
 			});
